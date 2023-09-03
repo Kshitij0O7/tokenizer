53b69f05085e73aba6f914fa5d3e7ef4bb12cc27
Kshitij <kshitij.mahajan.20042@iitgoa.ac.in>
2023-09-03 21:34:39 +0530

    Initial Commit

---
+++ scripts/program.js
@@ -0,0 +1,9 @@
+import { exec } from 'child_process';
+var yourscript = exec('sh deploy.sh',
+        (error, stdout, stderr) => {
+            console.log(stdout);
+            console.log(stderr);
+            if (error !== null) {
+                console.log(`exec error: ${error}`);
+            }
+        });
